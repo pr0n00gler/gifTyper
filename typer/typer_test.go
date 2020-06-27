9a2384f822ba9e859ba408a975b4ec117780e83f
uncolor <sapozhnikov.v@postuf.com>
2020-06-27 14:06:59 +0300

    Change test dividing text on lines

--- typer/typer_test.go
+++ typer/typer_test.go
@@ -1,48 +1,41 @@
 package typer
 
 import (
-	"reflect"
 	"testing"
 )
 
 type DivideTextTestCase struct {
 	text        string
-	lines       []string
+	linesCount  int
 	framesCount int
 }
 
 func TestDivideTextOnLines(t *testing.T) {
-	const (
-		W             = 500
-		H             = 250
-		maxLineSize   = 5
-		maxLinesCount = 5
-	)
+	generator, err := InitGenerator()
+	if err != nil {
+		t.Error(err.Error())
+		return
+	}
 
 	testCases := []DivideTextTestCase{
 		{"This is a test line",
-			[]string{"This ", "is a ", "test ", "line"},
+			1,
 			19,
 		},
 	}
 
-	generator := &Typer{
-		maxLinesCount: maxLinesCount,
-		maxLineSize:   maxLineSize,
-		frameW:        W,
-		frameH:        H,
+	testCase := testCases[0]
+	lines, framesCount, err := generator.divideTextOnLines(testCase.text)
+	if err != nil {
+		t.Error(err.Error())
+		return
 	}
 
-	for _, testCase := range testCases {
-		lines, framesCount, err := generator.divideTextOnLines(testCase.text)
-		if err != nil {
-			t.Error(err.Error())
-		}
-		if framesCount != testCase.framesCount {
-			t.Errorf("number of frames is not right: %v != %v", framesCount, testCase.framesCount)
-		}
-		if !reflect.DeepEqual(lines, testCase.lines) {
-			t.Errorf("failed: %v != %v", lines, testCase.lines)
-		}
+	if framesCount != testCase.framesCount {
+		t.Errorf("number of frames is not right: %v != %v", framesCount, testCase.framesCount)
+	}
+
+	if testCase.linesCount != len(lines) {
+		t.Errorf("failed: %v != %v", testCase.linesCount, len(lines))
 	}
 }
